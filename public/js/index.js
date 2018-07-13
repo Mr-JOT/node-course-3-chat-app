f84bd91e63623e50a265916ea4f26b01af87073f
Your Name <you@example.com>
2018-07-13 05:33:03 +0100

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,10 +29,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit',function (e) {
     e.preventDefault();
 
+    var messageTextBox = jQuery('[name=message]');
+
     socket.emit('createMessage',{
        from: 'User',
-       text:jQuery('[name=message]').val()
+       text:messageTextBox.val()
     }, function (){
+        messageTextBox.val('')
 
     });
 });
@@ -44,13 +47,18 @@ jQuery('#message-form').on('submit',function (e) {
            return alert('Geolocation not supported by your browser.');
         }
 
+        locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+
         navigator.geolocation.getCurrentPosition(function (position){
-           socket.emit('createLocationMessage', {
+          locationButton.removeAttr('disabled').text('Send location');
+            socket.emit('createLocationMessage', {
              latitude:position.coords.latitude,
              longitude:position.coords.longitude
            });
 
         }, function () {
+            locationButton.removeAttr('disabled').text('Send location');
           alert('Unable to fetch location.');   
         });
 
